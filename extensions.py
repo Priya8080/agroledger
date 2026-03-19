d3fa98e80ea5d0311a0e049cd8e1dcfb03091d1b
Priya Kumari <kumaripriya8084785476@gmail.com>
2026-03-19 19:22:54 +0530

    add

--- extensions.py
+++ extensions.py
@@ -1,3 +1,31 @@
-from flask_mysqldb import MySQL
+import pymysql
+from flask import g, current_app
+
+class MySQL:
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.teardown_appcontext(self.teardown)
+
+    @property
+    def connection(self):
+        if 'mysql_db' not in g:
+            port = int(current_app.config.get('MYSQL_PORT', 3306))
+            g.mysql_db = pymysql.connect(
+                host=current_app.config.get('MYSQL_HOST', 'localhost'),
+                user=current_app.config.get('MYSQL_USER', 'root'),
+                password=current_app.config.get('MYSQL_PASSWORD', ''),
+                db=current_app.config.get('MYSQL_DB', ''),
+                port=port,
+                autocommit=True
+            )
+        return g.mysql_db
+
+    def teardown(self, exception):
+        db = g.pop('mysql_db', None)
+        if db is not None:
+            db.close()
 
 mysql = MySQL()
